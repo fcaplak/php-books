ca43d920cb5ada1035d34c66da079c99053dc8d1
Filip Caplak <f.caplak@centrum.sk>
2021-02-11 02:14:49 +0100

    edited headers

--- backend/src/api/api_include.php
+++ backend/src/api/api_include.php
@@ -1,4 +1,8 @@
 <?php
+	header("Access-Control-Allow-Headers: *");
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: GET, POST");  
+	header('Content-Type: application/json');
+    
     require_once('config.php');
-    header('Content-Type: application/json');
 ?>
--- backend/src/api/config.php
+++ backend/src/api/config.php
@@ -9,4 +9,6 @@
     $conn = new mysqli($server_name, $user_name, $password, $db_name);
 
     if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+
+    mysqli_set_charset($conn, 'utf8');
 ?>
